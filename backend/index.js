09a7670e11a917e32bf18e5534246f449c07feb4
Aizen200 <yatinbisht407@gmail.com>
2026-05-27 23:24:20 +0530

    Inital setup

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express=require("express")
+const mongoose=require("mongoose")
+const dotenv=require("dotenv")
+const app= express()
+dotenv.config()
+app.use(express.json())
+mongoose.connect(process.env.mongo_uri)
+.then(()=>console.log("Mongodb Connected")).catch((err)=>console.log(err))
+
+app.get("/",(req,res)=>{
+    res.status(200).send("Server running")
+})
+app.listen(3000,()=>{
+    console.log("Server running")
+})
