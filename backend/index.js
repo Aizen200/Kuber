48c2f54d7ef630e3883b4b06f8d2416c6c913de4
Aizen200 <yatinbisht407@gmail.com>
2026-06-23 12:46:25 +0530

    Created user shcmea

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const dotenv=require("dotenv")
 const app= express()
 dotenv.config()
 app.use(express.json())
+app.use(cors())
 mongoose.connect(process.env.mongo_uri)
 .then(()=>console.log("Mongodb Connected")).catch((err)=>console.log(err))
 
